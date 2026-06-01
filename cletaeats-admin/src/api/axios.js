01e25f7d745525097d6b117270d1bf2b4623239c
Alejandro <alejandro.alvarez.chaves@est.una.ac.cr>
2026-05-31 19:32:41 -0600

    Cambios

--- cletaeats-admin/src/api/axios.js
+++ cletaeats-admin/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://cletaeats.onrender.com",
+    baseURL: "https://cletaeatsapp.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
